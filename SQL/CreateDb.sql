b75db44a1d5d653b05a1f5a3ffabd93feb94ddf1
Alex Aimson <146964667+Aimsona@users.noreply.github.com>
2024-11-07 09:29:09 +0000

    feat: update query to be able to run in sqlite

--- SQL/CreateDb.sql
+++ SQL/CreateDb.sql
@@ -1,78 +1,54 @@
 CREATE TABLE Person (
-    Id INT NOT NULL IDENTITY(1,1),
-    [Name] NVARCHAR(255) NOT NULL,
-    CONSTRAINT PK_Person PRIMARY KEY (Id)
+    Id INTEGER PRIMARY KEY NOT NULL,
+    [Name] NVARCHAR(255) NOT NULL
 );
 
 CREATE TABLE [Order](
-	Id INT NOT NULL IDENTITY(1,1),
+	Id INTEGER PRIMARY KEY NOT NULL,
 	PersonId INT NOT NULL,
 	[Date] DATETIME NOT NULL,	
-	CONSTRAINT PK_Order PRIMARY KEY (Id),
 	CONSTRAINT FK_Person FOREIGN KEY (PersonId) REFERENCES Person(Id)
 );
 
 CREATE TABLE Product(
-	Id INT NOT NULL IDENTITY(1,1),
+	Id INTEGER PRIMARY KEY NOT NULL,
 	[Name] NVARCHAR(500) NOT NULL,
-	Cost DECIMAL(19,2) NOT NULL,
-	CONSTRAINT PK_Product PRIMARY KEY (Id)
+	Cost DECIMAL(19,2) NOT NULL
 );
 
 CREATE TABLE OrderItem(
-	Id INT NOT NULL IDENTITY(1,1),
+	Id INTEGER PRIMARY KEY NOT NULL,
 	OrderId INT NOT NULL,
 	ProductId INT NOT NULL,
 	CostAtTimeOfSale DECIMAL(19,2) NOT NULL,
 	NumberOfItems INT NOT NULL,
-	CONSTRAINT PK_OrderItem PRIMARY KEY (Id),
 	CONSTRAINT FK_Order FOREIGN KEY (OrderId) REFERENCES [Order](Id),
 	CONSTRAINT FK_Product FOREIGN KEY (ProductId) REFERENCES Product(Id)
 );
 
-INSERT INTO Person VALUES ('Steve');
-DECLARE @steveId INT = SCOPE_IDENTITY();
+INSERT INTO Person (Id, Name) VALUES (1, 'Steve');
+INSERT INTO Person (Id, Name) VALUES (2, 'Tom');
+INSERT INTO Person (Id, Name) VALUES (3, 'Gary');
 
-INSERT INTO Person VALUES ('Tom');
-DECLARE @tomId INT = SCOPE_IDENTITY();
+INSERT INTO Product (Id, Name, Cost) VALUES (1, 'Monitor', 200); 
+INSERT INTO Product (Id, Name, Cost) VALUES (2, 'CPU', 150);
+INSERT INTO Product (Id, Name, Cost) VALUES (3, 'RAM', 50);
+INSERT INTO Product (Id, Name, Cost) VALUES (4, 'Motherboard', 100);
 
-INSERT INTO Person VALUES ('Gary');
-DECLARE @garyId INT = SCOPE_IDENTITY();
+INSERT INTO [Order] (Id, PersonId, [Date]) VALUES (1, 2, '2019-12-05'); -- Tom Order 1
+INSERT INTO [Order] (Id, PersonId, [Date]) VALUES (2, 2, '2019-12-20'); -- Tom Order 2
+INSERT INTO [Order] (Id, PersonId, [Date]) VALUES (3, 1, '2019-12-06'); -- Steve Order
+INSERT INTO [Order] (Id, PersonId, [Date]) VALUES (4, 3, '2019-12-07'); -- Gary Order
 
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (1, 1, 2, 170.00, 2);  -- Tom Order 1
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (2, 1, 4, 100.00, 2);  -- Tom Order 1
 
-INSERT INTO Product VALUES ('Monitor', 200);
-DECLARE @monitorId INT = SCOPE_IDENTITY();
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (3, 2, 3, 170.00, 2);  -- Tom Order 2
 
-INSERT INTO Product VALUES ('CPU', 150);
-DECLARE @CPUId INT = SCOPE_IDENTITY();
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (4, 3, 2, 150.00, 1); -- Steve Order
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (5, 3, 4, 100.00, 1); -- Steve Order 
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (6, 3, 3, 50.00, 2);  -- Steve Order 
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (7, 3, 1, 180.50, 2); -- Steve Order
 
-INSERT INTO Product VALUES ('RAM', 50);
-DECLARE @RAMId INT = SCOPE_IDENTITY();
-
-INSERT INTO Product VALUES ('Motherboard', 100);
-DECLARE @MotherboardId INT = SCOPE_IDENTITY();
-
-INSERT INTO [Order] VALUES (@tomId, '2019-12-05');
-DECLARE @tomOrder1 INT = SCOPE_IDENTITY();
-
-INSERT INTO [Order] VALUES (@tomId, '2019-12-20');
-DECLARE @tomOrder2 INT = SCOPE_IDENTITY();
-
-INSERT INTO [Order] VALUES (@steveId, '2019-12-06');
-DECLARE @steveOrder INT = SCOPE_IDENTITY();
-
-INSERT INTO [Order] VALUES (@garyId, '2019-12-07');
-DECLARE @garyOrder INT = SCOPE_IDENTITY();
-
-INSERT INTO OrderItem VALUES (@tomOrder1, @CPUId, 170.00, 2);
-INSERT INTO OrderItem VALUES (@tomOrder1, @MotherboardId, 100.00, 2);
-
-INSERT INTO OrderItem VALUES (@tomOrder2, @RAMId, 170.00, 2);
-
-INSERT INTO OrderItem VALUES (@steveOrder, @CPUId, 150.00, 1);
-INSERT INTO OrderItem VALUES (@steveOrder, @MotherboardId, 100.00, 1);
-INSERT INTO OrderItem VALUES (@steveOrder, @RAMId, 50.00, 2);
-INSERT INTO OrderItem VALUES (@steveOrder, @monitorId, 180.50, 2);
-
-INSERT INTO OrderItem VALUES (@garyOrder, @RAMId, 50.00, 2);
-INSERT INTO OrderItem VALUES (@garyOrder, @monitorId, 180.50, 3);
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (8, 4, 3, 50.00, 2);  -- Gary Order 
+INSERT INTO OrderItem (Id, OrderId, ProductId, CostAtTimeOfSale, NumberOfItems)  VALUES (9, 4, 1, 180.50, 3); -- Gary Order 
